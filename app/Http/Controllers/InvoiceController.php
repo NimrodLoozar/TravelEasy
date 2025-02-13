1b462905ae8f27668f35300e004a23e116c34c8c
ThomasTadesse <328704@student.mboutrecht.nl>
2025-02-13 11:42:22 +0100

    invoiceController fix

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,20 +2,13 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
-=======
 use App\Models\Invoice;
 // use App\Models\Patient;
 // use App\Models\Treatment;
->>>>>>> origin/feature
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-<<<<<<< HEAD
-    //
-}
-=======
     /**
      * Toon de lijst met facturen.
      */
@@ -146,4 +139,3 @@ class InvoiceController extends Controller
         return response()->json(['nextNumber' => str_pad($nextNumber, 6, '0', STR_PAD_LEFT)]);
     }
 }
->>>>>>> origin/feature
