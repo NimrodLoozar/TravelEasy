1d0a6fa30b29666efe16aa3f0eca657ea26f94e9
NimrodLobozar <nimrod.lobozar@gmail.com>
2025-02-13 10:47:01 +0100

    ``` No code changes made; skipping commit. ```

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,6 +11,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('invoice.index')" :active="request()->routeIs('invoice.index')">
+                        {{ __('Factuuren') }}
+                    </x-nav-link>
                 </div>
             </div>
 
