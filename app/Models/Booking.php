3cc61fb8e13a74b1c5c236f707f97aab8a99d13a
ThomasTadesse <328704@student.mboutrecht.nl>
2025-02-21 11:59:47 +0100

    models fix

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Customer;
 use App\Models\Trip;
@@ -9,6 +10,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Booking extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'customer_id',
         'trip_id',
