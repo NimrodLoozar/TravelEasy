561df4d8270a9301051322857037ccafbec9a5bb
Martijn339074 <339074@student.mboutrecht.nl>
2025-04-04 13:34:11 +0200

    Fixed create bug

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -21,7 +21,7 @@ class Employee extends Model
     {
         return $this->belongsTo(Person::class);
     }
-
+ 
     public function roles()
     {
         return $this->belongsToMany(Role::class);
